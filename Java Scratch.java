055c54a2bb07846af6de68a0433f8cfc3429bae3
rafszera <114614854+rafszera@users.noreply.github.com>
2023-02-03 14:01:05 -0300

    att

--- Java Scratch.java
+++ Java Scratch.java
@@ -1,72 +1,71 @@
-import java.util.jar.Attributes.Name;
-
+import java.util.Scanner;
 class Java_Scratch {
   
     public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.println("Hello");
+    //System.out.println("Hello world!");
+    //System.out.println("Hello");
     
-      int age = 25;
-      System.out.println("I am " + age + " years old");      
+      //int age = 25;
+      //System.out.println("I am " + age + " years old");      
 
-      char percentSymbol = '%';
-      System.out.println("This is percent symbol " + percentSymbol);
+      //char percentSymbol = '%';
+      //System.out.println("This is percent symbol " + percentSymbol);
 
-      String lul = "kek";
-      System.out.println(lul + " bur");
+      //String lul = "kek";
+      //System.out.println(lul + " bur");
       
-      double number1 = 20;
-      double number2 = 40;
-      System.out.println(number1 * number2);
-      System.out.println(number1 + number2);
-      System.out.println(number1 - number2);
-      System.out.println(number1 / number2);
+      //double number1 = 20;
+      //double number2 = 40;
+      //System.out.println(number1 * number2);
+      //System.out.println(number1 + number2);
+      //System.out.println(number1 - number2);
+      //System.out.println(number1 / number2);
 
-      int number3 = 93;
-      double number4 = number3;
-      System.out.println(number4);   
+      //int number3 = 93;
+      //double number4 = number3;
+      //System.out.println(number4);   
       
-      number3 = number3 + 20;
-      System.out.println(number3);
+      //number3 = number3 + 20;
+      //System.out.println(number3);
       
-      number3 += 20;
-      System.out.println(number3);
+      //number3 += 20;
+      //System.out.println(number3);
       
-      int number5 = 10;
-      int number6 = 20; 
-      int number7 = 50;
-      int number8 = 50;
+      //int number5 = 10;
+      //int number6 = 20; 
+      //int number7 = 50;
+      //int number8 = 50;
       
       // is equal
-      System.out.println(number5==number6); 
-      System.out.println(number7==number8);
+      //System.out.println(number5==number6); 
+      //System.out.println(number7==number8);
       // is not equal
-      System.out.println(number7!=number5);
+      //System.out.println(number7!=number5);
       // is greater than
-      System.out.println(number7>number6);
+      //System.out.println(number7>number6);
       // is less than
-      System.out.println(number6<number5);
+      //System.out.println(number6<number5);
       // is greater than or equal to
-      System.out.println(number8>=number7);
+      //System.out.println(number8>=number7);
       // is less than or equal to
-      System.out.println(number5<=number6);
+      //System.out.println(number5<=number6);
 
-      System.out.println(("####"));
-      age = 25;
+      //System.out.println(("####"));
+      //age = 25;
       // age >=18
       // age <=40
-      System.out.println(age >= 18 && age <=40);
-      age = 45;
-      System.out.println(age >= 18 && age <=40);
+      //System.out.println(age >= 18 && age <=40);
+      //age = 45;
+      //System.out.println(age >= 18 && age <=40);
      
-      System.out.println(("###"));
+      //System.out.println(("###"));
 
-      boolean isStudent = false;
-      boolean isLibraryMember = true;
-      System.out.println(isStudent || isLibraryMember);
-      System.out.println(!isStudent);
+      //boolean isStudent = false;
+      //boolean isLibraryMember = true;
+      //System.out.println(isStudent || isLibraryMember);
+      //System.out.println(!isStudent);
 
-      System.out.println(("###"));
+      //System.out.println(("###"));
 
       int score = 0;
       int turns = 10;
@@ -77,50 +76,48 @@ class Java_Scratch {
       System.out.println("pontuação " + score);
       System.out.println("turnos " + turns);
 
-      int number = 55;
-      System.out.println(--number);
+      //int number = 55;
+      //System.out.println(--number);
 
-      System.out.println(("###"));
+      //System.out.println(("###"));
 
-      String FullName = "Rafael Saibro ";
-      System.out.println(FullName);
+      //String FullName = "Rafael Saibro ";
+      //System.out.println(FullName);
 
-      // Testando GitHub
-      // Test 2
 
-      String literalString1 = "ABC";
-      String literalString2 = "ABC";
+      //String literalString1 = "ABC";
+      //String literalString2 = "ABC";
 
-      String objectString1 = new String("xyz");
-      String objectString2 = new String("xyz");
+      // String objectString1 = new String("xyz");
+      // String objectString2 = new String("xyz");
 
-      System.out.println(literalString1 == literalString2);
-      System.out.println(objectString1 == objectString2);
+      // System.out.println(literalString1 == literalString2);
+      // System.out.println(objectString1 == objectString2);
 
-      System.out.println(("###"));
+      // System.out.println(("###"));
 
-      System.out.println("HELLO! I am Rafael. I am from Brazil and I am 25 years old.");
+      // System.out.println("HELLO! I am Rafael. I am from Brazil and I am 25 years old.");
 
-      String name = "Rafael"; 
-      String country = "Brazil";
-      age = 25;
+      //String name = "Rafael"; 
+      //String country = "Brazil";
+      //age = 25;
       
-      System.out.println("Hello! I am " + name + ". I am from " + country + " and I am " + age + " years old. ");
+      //System.out.println("Hello! I am " + name + ". I am from " + country + " and I am " + age + " years old. ");
 
-      String formattedString = String.format("hello! I am %s. I am from %s and I am %d years old.", name, country, age);
-      System.out.println(formattedString);
-      System.out.println("Tem " + name.length() + " letras no meu nome");
-      name = "";
-      System.out.println(name.isEmpty());
-      name = "Rafa";
-      System.out.println(name.toUpperCase());
-      System.out.println(name.toLowerCase());
+      //String formattedString = String.format("hello! I am %s. I am from %s and I am %d years old.", name, country, age);
+      //System.out.println(formattedString);
+      //System.out.println("Tem " + name.length() + " letras no meu nome");
+      //name = "";
+      //System.out.println(name.isEmpty());
+      //name = "Rafa";
+      //System.out.println(name.toUpperCase());
+      //System.out.println(name.toLowerCase());
       
-      String string1 = new String("qwerty");
-      String string2 = new String("QWERTY");
-      System.out.println(string1 == string2);
-      System.out.println(string1.equals(string2));
-      System.out.println(string1.equalsIgnoreCase(string2));
+      // String string1 = new String("qwerty");
+      // String string2 = new String("QWERTY");
+      // System.out.println(string1 == string2);
+      // System.out.println(string1.equals(string2));
+      // System.out.println(string1.equalsIgnoreCase(string2));
       
       String ceu = "The sky is blue.";
       System.out.println(ceu.replace("blue", "red"));
@@ -129,6 +126,32 @@ class Java_Scratch {
       System.out.println(updatedCeu);
 
       System.out.println(ceu.contains("sky"));
+
+
+      Scanner scanner = new Scanner(System.in);
+
+
+      System.out.printf("Whats is your name? ");
+      String name = scanner.nextLine();
+
+      System.out.printf("Hello %s, How old are you? ", name);
+      int age = Integer.parseInt(scanner.nextLine());
+      
+      //cleans up the input buf
+      //scanner.nextLine()
+                   
+      System.out.printf("%d is an excellent age to start programming. What language do you prefer? ", age);
+      String language = scanner.nextLine();
+      
+      System.out.printf("%s is a very nice popular programming language.", language);
+
+      scanner.close();
+
+
+
+
+
+
 
 
 
@@ -205,6 +228,8 @@ class Java_Scratch {
       // git add .
       // git status
       // git commit -m " ""
+
+  
 
 
     }
